de0adb75bd919957d142b714672bc79a13a9f377
Donovan Block <thegreatcatadorer@gmail.com>
2023-02-10 21:15:44 -0500

    it works now, not that that makes it useful

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct Command {
     dest: String,
 }
 
-fn parse(string: String, actions: &mut Vec<Command>) {
+fn parse(string: &str, actions: &mut Vec<Command>) {
     for line in string.lines() {
         let (variable, rest) = parse_word(line, '=');
         let (action, mut rest) = parse_word(rest, '(');
@@ -30,7 +30,7 @@ fn parse(string: String, actions: &mut Vec<Command>) {
 
 fn parse_word(string: &str, delim: char) -> (&str, &str) {
     let delim = string.find(delim).unwrap_or(string.len());
-    ((&string[0..delim]).trim(), &string[delim..string.len()])
+    ((&string[0..delim]).trim(), &string[delim + 1..string.len()])
 }
 
 fn parse_subst(string: &str) -> Subst {
@@ -56,42 +56,50 @@ fn parse_subst(string: &str) -> Subst {
 
 const HEADER: &str = "#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <spawn.h>
-char *run(const char *restrict file, char *const argv[restrict]){
-int status, len, procout[2], pid_t pid, posix_spawn_file_actions_t acts, char *result;
-if (status = pipe(procout)) exit(status);
-posix_spawn_file_actions_init(&acts);
-posix_spawn_file_actions_adddup2(&acts, procout[1], STDOUT_FILENO);
-if (status = posix_spawnp(&pid, file, &acts, NULL, argv, environ)) exit(status);
-while (waitpid(pid, &status, 0) == -1);
-posix_spawn_file_actions_destroy(&acts);
-if (status = WEXITSTATUS(status)) exit(status);
-result = malloc(len = lseek(procout[0], 0, SEEK_END));
-lseek(procout[0], 0, SEEK_SET);
-if (result == NULL) exit(1);
-fread(result, 1, len, procout[0]);
-close(procout[0]); close(procout[1]);
-return result;}
-";
+extern char**environ;\
+char*run(const char*restrict file,char*const argv[restrict]){\
+int status,len,procout[2];pid_t pid;posix_spawn_file_actions_t acts;char*result;\
+if((status=pipe(procout)))exit(status);\
+posix_spawn_file_actions_init(&acts);\
+posix_spawn_file_actions_adddup2(&acts,procout[1],STDOUT_FILENO);\
+if((status=posix_spawnp(&pid,file,&acts,NULL,argv,environ)))exit(status);\
+while(waitpid(pid,&status,0)==-1){}\
+posix_spawn_file_actions_destroy(&acts);\
+if((status=WEXITSTATUS(status)))exit(status);\
+result=malloc(len=lseek(procout[0],0,SEEK_END));\
+lseek(procout[0],0,SEEK_SET);\
+if(result==NULL)exit(1);\
+fread(result,1,len,fdopen(procout[0], \"r\"));\
+close(procout[0]);close(procout[1]);\
+return result;}\
+int main(void){";
 
 fn output(actions: Vec<Command>) -> String {
     let mut result = HEADER.to_string();
     for Command { func, args, dest } in actions {
+        result.push('\n');
+        result.push('{');
         for (i, subst) in args.iter().enumerate() {
             conc(subst, &format!("local_{i}"), &mut result);
         }
         conc(&func, "func", &mut result);
-        result.push_str("char *");
+        write!(&mut result, "char*args[{}]=", args.len() + 1).unwrap();
+        format_list(
+            |i, buf| write!(buf, "local_{i}").unwrap(),
+            0..args.len(),
+            "(char*)NULL",
+            &mut result,
+        );
+        result.push_str("char*");
         result.push_str(&dest);
-        result.push_str(" = run(func,");
-        let mut args = 0..args.len();
-        while let Some(i) = args.next() {
-            write!(&mut result, "local_{i}").unwrap();
-            result.push(if args.len() == 0 { ')' } else { ',' });
-        }
+        result.push_str("=run(func,args);}");
     }
+    result.push('}');
     result
 }
 
@@ -107,23 +115,27 @@ fn write_literal(subst: &Result<String, String>, buf: &mut String) {
 }
 
 fn decl_str(dest: &str, string: &Result<String, String>) -> String {
-    let mut result = "char *".to_string();
+    let mut result = "char*".to_string();
     result.push_str(dest);
-    result.push_str(" = ");
+    result.push('=');
     write_literal(&string, &mut result);
     result.push(';');
     return result;
 }
 
-fn format_list<T>(mut write: impl FnMut(&T, &mut String) -> (), args: &[T], buf: &mut String) {
-    let mut items = args.iter();
+fn format_list<T>(
+    mut write: impl FnMut(T, &mut String) -> (),
+    mut items: impl Iterator<Item = T>,
+    suffix: &str,
+    buf: &mut String,
+) {
+    buf.push('{');
     while let Some(item) = items.next() {
         write(item, buf);
         buf.push(',');
-        if items.len() == 0 {
-            buf.push_str("NULL};");
-        }
     }
+    buf.push_str(suffix);
+    buf.push_str("};");
 }
 
 fn conc(subst: &Subst, dest: &str, mut result: &mut String) {
@@ -131,26 +143,26 @@ fn conc(subst: &Subst, dest: &str, mut result: &mut String) {
         result.push_str(&decl_str(dest, &string));
         return;
     }
-    result.push_str("int *lengths = {");
+    write!(&mut result, "int lengths[{}]=", subst.len() + 1).unwrap();
     fn write_length(item: &Result<String, String>, buf: &mut String) {
         buf.push_str("strlen(");
         write_literal(item, buf);
-        buf.push_str(")");
+        buf.push(')');
     }
-    format_list(write_length, &subst, &mut result);
-    result.push_str("char **substrings = {");
-    format_list(write_literal, &subst, &mut result);
+    format_list(write_length, subst.iter(), "0", &mut result);
+    write!(&mut result, "char*substrings[{}]=", subst.len() + 1).unwrap();
+    format_list(write_literal, subst.iter(), "0", &mut result);
     write!(
         &mut result,
-        "int length = 0; for (int i = 0; i < {};) length += lengths[i++]",
+        "int length=0;for(int i=0;i<{};)length+=lengths[i++];",
         subst.len()
     )
     .unwrap();
     write!(
         &mut result,
-        "char *{dest} = malloc(length); if ({dest} == NULL) exit(1);\
-        for (int i = 0, char *ref = {dest}; i < {};)\
-        {{ memcpy(ref, substrings[i], lengths[i]); ref += lengths[i++]; }}",
+        "char*{dest}=malloc(length);if({dest}==NULL)exit(1);\
+        char*ref={dest};for(int i=0;i<{};)\
+        (memcpy(ref,substrings[i],lengths[i]),ref+=lengths[i++]);",
         subst.len()
     )
     .unwrap();
@@ -199,7 +211,7 @@ fn main() {
     let mut actions = Vec::new();
     for arg in args {
         let contents = std::fs::read_to_string(Path::new(&arg)).expect("file not found!");
-        parse(contents, &mut actions);
+        parse(&contents, &mut actions);
     }
     println!("{}", output(actions));
 }
